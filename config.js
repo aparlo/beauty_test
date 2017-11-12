cc3e76e0a55a36a93c54cd8368c327921fb6cb70
aparlo <aparlo@yandex.ru>
2017-11-12 23:49:22 +0300

    v0.9 SendSMS, chat

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ var url = 'mongodb://localhost:27017/testing-new'
 var session = require('express-session')
 var mongoose = require('mongoose')
 var MongoStore = require('connect-mongo')(session)
- 
+
 //Session config
 var sessionMiddleware = session({
     store: new MongoStore({url:url}),
